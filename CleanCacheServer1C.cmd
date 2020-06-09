e308e90516f79d41e86ac8a57120b1902b13f1df
Федор Кубанец <f.kubanets@happylook.ru>
2020-06-10 01:07:34 +0300

    Добавил в CMD варианты запуска с ключами

--- CleanCacheServer1C.cmd
+++ CleanCacheServer1C.cmd
@@ -1,1 +1,5 @@
-@powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1
+@Echo OFF
+REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -RebootHost
+REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -NoRestart
+REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1
+powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 %*
