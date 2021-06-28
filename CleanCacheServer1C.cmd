95b84dd31fad2b32482fff1ec8f1d4d0d99c80fc
Fedor Kubanets <f.kubanets@happylook.ru>
2021-06-28 22:46:46 +0300

    Change example in batch CleanCacheServer1C

--- CleanCacheServer1C.cmd
+++ CleanCacheServer1C.cmd
@@ -2,4 +2,5 @@
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5 -RebootHost
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5 -NoRestart
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5
+REM powershell -ExecutionPolicy Bypass -File %~dp0CleanCacheServer1C.ps1 -ServerPath "C:\V8SRVDATA" -Pause1 30 -Pause2 5 -CleanConnectionProfile %*
 powershell -ExecutionPolicy Bypass -File %~dp0CleanCacheServer1C.ps1 -ServerPath "D:\V8SRVDATA" -Pause1 30 -Pause2 5 %*
