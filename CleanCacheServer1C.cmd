f5ecd49bb030b6e4a4a3ce780b7a03436d3126f5
Fedor Kubanets <f.kubanets@happylook.ru>
2022-12-03 13:04:03 +0300

    Change console locale to EN_us

--- CleanCacheServer1C.cmd
+++ CleanCacheServer1C.cmd
@@ -1,4 +1,5 @@
 @Echo OFF
+chcp 437
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5 -RebootHost
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5 -NoRestart
 REM powershell -ExecutionPolicy Bypass -File CleanCacheServer1C.ps1 -ServerPath "E:\V8SRVDATA" ProfilePath "C:\Users\server1c" -PFLPath "C:\ProgramData\1C\1cv8" -Pause1 20 -Pause2 5
